bdbb531ccf54463a3fb0603a4f886d7570f9ef67
titosco <titobiloluwatella@gmail.com>
2024-10-24 09:12:59 +0100

    final link

--- crestville/app/about/page.jsx
+++ crestville/app/about/page.jsx
@@ -2,7 +2,14 @@ import React from "react";
 
 const About = () => {
   return (
-    <main className="mt-28 sm:mt-36">About</main>
+    <main className="mt-28 sm:mt-36">
+      <div className="flex flex-col gap-6 p-28 px-3 max-w-6xl mx-auto font-header">
+        <h1 className=" font-bold text-3xl lg:text-6xl">Find you the <span className="text-primary">Prefect</span>
+        <br />
+        shortlet with ease
+        </h1>
+      </div>
+    </main>
   );
 };
 
