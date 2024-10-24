3be832546941ce7d8affba1cf44dfdd781fe9d13
titosco <titobiloluwatella@gmail.com>
2024-10-24 22:21:28 +0100

    setting up slides

--- crestville/app/about/page.jsx
+++ crestville/app/about/page.jsx
@@ -1,14 +1,25 @@
+import Link from "next/link";
 import React from "react";
+import Slide from './slide';
 
 const About = () => {
   return (
     <main className="mt-28 sm:mt-36">
-      <div className="flex flex-col gap-6 p-28 px-3 max-w-6xl mx-auto font-header">
-        <h1 className=" font-bold text-3xl lg:text-6xl">Find you the <span className="text-primary">Prefect</span>
+      <div className="flex flex-col gap-6 p-8 px-3 max-w-6xl mx-auto font-header md:p-14 md:mx-16 lg:mx-8 lg:p-28 ">
+        <h1 className=" font-bold text-3xl lg:text-6xl">Find you the <span className="text-primary">Perfect</span>
         <br />
         shortlet with ease
         </h1>
+        <div className="text-gray-400 text-xs sm:text-sm">Crestville Apartment is the perfect apartment for you to live.
+          <br />
+          We have a wide range of properties for you to choose from.
+        </div>
+        <Link href={`/listings`}
+              className="text-xs sm:text-sm text-primary font-bold hover:underline">
+          Let&apos;s get started....
+        </Link>
       </div>
+      <Slide />
     </main>
   );
 };
---
+++ crestville/app/about/slide.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+// import {Swiper,SwiperSlide } from 'swiper/react';
+
+// import 'swiper/css';
+// import 'swiper/css/effect-coverflow';
+// import 'swiper/css/pagination';
+// import 'swiper/css/navigation';
+
+const slide = () => {
+  return <div>
+        slidess
+  </div>;
+};
+
+export default slide;
